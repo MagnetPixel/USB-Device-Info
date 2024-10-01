f970ad0924cf6bde7e7bf5f8e10b63e43b6911e8
MagnetPixel <daniel.bernas@icloud.com>
2024-10-02 00:46:56 +0200

    Added OSX basic support

--- USBDeviceInfo.h
+++ USBDeviceInfo.h
@@ -1,29 +1,47 @@
 #ifndef USBDEVICEINFO_H
 #define USBDEVICEINFO_H
 
-#include <windows.h>
-#include <setupapi.h>
+
 #include <string>
 #include <iostream>
 
+#ifdef _WIN32
+#include <windows.h>
+#include <setupapi.h>
+#endif
+
+#ifdef __APPLE__
+#include <IOKit/IOKitLib.h>
+#include <CoreFoundation/CoreFoundation.h>
+#endif
+
 class USBDeviceInfo {
 public:
-    // Konstruktorn
+    // Construktor
     USBDeviceInfo();
-    // Destruktorn
+    // Destruktor
     ~USBDeviceInfo();
 
-    // Funktion för att hämta och visa alla USB-enheter
+    // function to get all USB devices
     void listUSBDevices();
 
 private:
-    // Hjälpfunktioner för att få specifik information om en enhet
+    #ifdef _WIN32
+    // Helpfunctions
     std::string getDeviceProperty(HDEVINFO deviceInfoSet, SP_DEVINFO_DATA& deviceInfoData, DWORD property);
     std::string getDriverInfo(HDEVINFO deviceInfoSet, SP_DEVINFO_DATA& deviceInfoData);
     std::string getDriverVersion(HDEVINFO deviceInfoSet, SP_DEVINFO_DATA& deviceInfoData);
     std::string getDriverLocation(HDEVINFO deviceInfoSet, SP_DEVINFO_DATA& deviceInfoData);
     std::string getDeviceSpeed(HDEVINFO deviceInfoSet, SP_DEVINFO_DATA& deviceInfoData);
     std::string getUSBVersion(HDEVINFO deviceInfoSet, SP_DEVINFO_DATA& deviceInfoData);
+    #endif
+
+    #ifdef __APPLE__
+    // macOS-specific
+    void listUSBDevicesMac();
+    void printUSBDeviceInfo(io_object_t usbDevice);
+    std::string getPropertyString(io_object_t device, CFStringRef property);
+    #endif
 };
 
 #endif // USBDEVICEINFO_H
